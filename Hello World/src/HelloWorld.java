5b7c45ae9b9bc8fa328556431a594e58b7be4932
apcs3rd <APCS3rd@MHS-LT9-23>
2019-08-20 10:12:14 -0700

    Complete Hello World Project

---
+++ Hello World/src/HelloWorld.java
@@ -0,0 +1,13 @@
+//Nicolina Sandoval 
+//08/16/2019
+//period 3
+//First class (iconic) in Java for APCS A
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+
+	}
+
+}
