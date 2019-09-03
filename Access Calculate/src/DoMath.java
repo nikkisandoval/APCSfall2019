196370f53c2ffa61186d19c39b44f08a31205d1c
APCS3rd <APCS3rd@MHS-LT9-23>
2019-09-03 09:53:24 -0700

    pt0

---
+++ Access Calculate/src/DoMath.java
@@ -0,0 +1,13 @@
+//Nicolina Sandoval
+//APCS 3rd
+//August 29,2019
+//Call the code from the Calculate class /*(aka client or runner code/*
+public class DoMath {
+
+	public static void main(String[] args) {
+		System.out.println(Calculate.square(2));
+		System.out.println(Calculate.cube(2));
+
+	}
+
+}
